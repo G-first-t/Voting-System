7c07bf81223ede835b356f49c685f633da52bd34
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 15:07:20 +0300

    admin dashboard created

--- pollProject/pollApp/admin.py
+++ pollProject/pollApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Question,Choice
 
 # Register your models here.
 class PollAppAdmin(admin.AdminSite):
@@ -8,3 +9,17 @@ class PollAppAdmin(admin.AdminSite):
 
 admin_site=PollAppAdmin(name="pollApp")
 
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=4
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets=[
+        
+            ('section_1',{'fields':['question_text']}),
+            ('section_2',{'fields':['pub_date'],'classes':['collapse']}),
+        
+    ]
+    inlines=[ChoiceInline]
+
+admin_site.register(Question,QuestionAdmin)
