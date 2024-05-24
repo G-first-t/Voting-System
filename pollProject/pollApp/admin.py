f5f4fc1496fbe174cfb26c2efc210c838343ec4b
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 14:45:42 +0300

    changing the admin site

--- pollProject/pollApp/admin.py
+++ pollProject/pollApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class PollAppAdmin(admin.AdminSite):
+    site_title="The Voting System"
+    set_header="The Poll Mall"
+    index_title="The Voting System"
+
+admin_site=PollAppAdmin(name="pollApp")
+
--- pollProject/pollProject/urls.py
+++ pollProject/pollProject/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from pollApp.admin import admin_site
 
 
 urlpatterns=[
-    path('admin',admin.site.urls),
+    path('admin/',admin_site.urls),
 ]
