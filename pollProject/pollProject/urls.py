d45859b79db2289a716a59b05aabb341c31f2c37
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 14:33:07 +0300

    now starting with the django project

--- pollProject/pollProject/urls.py
+++ pollProject/pollProject/urls.py
@@ -1,13 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
-from blog.admin import blog_site
-from bookstore.admin import book_site
 
-urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('admin/', blog_site.urls),
-    path('bookadmin/',book_site.urls)
+
+urlpatterns=[
+    path('admin',admin.site.urls),
 ]
-
-
