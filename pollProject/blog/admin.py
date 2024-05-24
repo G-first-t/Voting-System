752be996ad541806aacbb32b4d1e9544ef3f46a7
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 11:58:47 +0300

    dealing with post

--- pollProject/blog/admin.py
+++ pollProject/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+class AdminArea(admin.AdminSite):
+    site_title="The Boss site"
+    index_title="site for editing"
+    site_header="admin dashboard"
+
+
+blog_site=AdminArea(name="BlogAdmin")
