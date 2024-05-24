3baf581c9f23a1afdae767f24720c78028fd06c7
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 10:44:09 +0300

    creating question model

--- pollProject/pollApp/models.py
+++ pollProject/pollApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text=models.CharField(max_length=255)
+    pub_date=models.DateTimeField('publishing date')
+
+    def __str__(self):
+        return self.question_text
