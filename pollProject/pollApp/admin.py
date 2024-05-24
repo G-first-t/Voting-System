d14d7e95b8b5aca38dfe3c779f25d621d723877c
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-24 14:49:53 +0300

    applying migrations

--- pollProject/pollApp/admin.py
+++ pollProject/pollApp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 class PollAppAdmin(admin.AdminSite):
     site_title="The Voting System"
-    set_header="The Poll Mall"
+    site_header="The Poll Mall"
     index_title="The Voting System"
 
 admin_site=PollAppAdmin(name="pollApp")
